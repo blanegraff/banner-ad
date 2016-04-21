954ff15bf2250686f0d2ef29f6b403113f09d2b1
blanegraff <brittany.lanegraff@mygeorgian.ca>
2016-04-21 18:48:30 -0400

    Added files via upload

---
+++ scripts/main.js
@@ -0,0 +1,61 @@
+ var count = 50;
+
+var path = new Path.Circle({
+	center: [0, 0],
+	radius: 10,
+	fillColor: '#c0392b',
+	strokeColor: '#c0392b'
+});
+
+var symbol = new Symbol(path);
+
+
+for (var i = 0; i < count; i++) {
+    
+	var center = Point.random() * view.size;
+	var placedSymbol = symbol.place(center);
+	placedSymbol.scale(i / count);
+}
+
+var text = new PointText({
+	point: view.center,
+	justification: 'center',
+	fontSize: 20,
+	fillColor: 'white',
+    
+    shadowColor: new Color(0, 0, 0),
+    // Set the shadow blur radius to 12:
+    shadowBlur: 18,
+    // Offset the shadow by { x: 5, y: 5 }
+    shadowOffset: new Point(5, 5)
+    
+});
+
+var destination = Point.random() * view.size;
+function onFrame(event) {
+	for (var i = 0; i < count; i++) {
+		var item = project.activeLayer.children[i];
+		
+		item.position.x += item.bounds.width / 20;
+
+		if (item.bounds.left > view.size.width) {
+			item.position.x = -item.bounds.width;
+		}
+	}
+    
+    var vector = destination - text.position;
+	
+	
+	text.position.x = 120;
+	
+	text.content = "Checkout My Projects";
+	text.onClick = function(event) {
+        OpenInNewTab("project.html");
+}
+    
+}
+
+function OpenInNewTab(url) {
+  var win = window.open(url, '_blank');
+  win.focus();
+}
